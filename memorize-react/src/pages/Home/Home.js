3b6bc1bc969652ce61f52b08450189d2d18b9598
DiegoColmenero <dicolmenero@terra.com.br>
2020-06-15 12:21:18 -0300

    up agoravai

--- memorize-react/src/pages/Home/Home.js
+++ memorize-react/src/pages/Home/Home.js
@@ -1,30 +1,40 @@
 import React from "react";
+import Cerebro from '../../assets/img/cerebro.png'
+import LogoMemorize from '../../assets/img/logo-memorize.png'
+import LogoColletivo from '../../assets/img/logo-colletivo.png'
+import OnibusColletivo from '../../assets/img/onibus-colletivo.png'
+import BtnIniciar from '../../assets/img/btn-iniciar.png'
 import {Link} from "react-router-dom";
 import "./Home.css";
 
 export default function Home(){
     return(
         <div className="Home">
-            <header className="main-header container">
-                <nav className="main-nav">
-                    <a href="#">Como jogar</a>
-                    <a href="#">Sobre</a>
-                    <a href="#">Colletivo Design</a>
-                </nav>
-                
-                {/* adicionar logo e imagem do cerebro */}
-                {/* <img />  */}
-                <p>Lorem ipsum dolor sit amet, consectetur adisciping elit</p> 
+            <section className='header'>
+                <header className="main-header-container">
+                    <nav className="main-nav">
+                        <a href="#">Como jogar</a>
+                        <a href="#">Sobre</a>
+                        <a href="#">Colletivo Design</a>
+                    </nav>
 
-                <span className="botao-jogar">
-                    {/* adicionar imagem do play */}
-                    {/* <img /> */}
-                    <p>Iniciar</p>
-                </span>               
-                
-                
-            </header>
-            <section className="como-jogar container">
+                    <div className='home-principal'>
+                        <span className='secreto'></span>
+                        <div className='home-logo'>
+                            <img src={LogoMemorize}/>
+                            <p className='sub-logo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra rutrum tempor!</p> 
+                            <br/>
+                            <span className="botao-jogar">
+                                <img src={BtnIniciar}/>
+                                <p>INICIAR</p>
+                            </span>               
+                        </div>
+                    </div>
+                    
+                    
+                </header>
+            </section>
+            <section className="como-jogar-container">
                 <h2>Como Jogar</h2>
                 <div>
                     <ul>
@@ -40,10 +50,24 @@ export default function Home(){
                     <p>Iniciar</p>
                 </span>    
             </section>
-            <section className="sobre container">
+            <section className="sobre-container">
+                <h1>Sobre</h1>
+                <h1>O que é?</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                <h1>Quem somos?</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
             </section>
-            <section className="colletivo container">
+            <section className="colletivo-container">
             </section>
+                <p>Este projeto é oferecido por</p>
+                <img src={LogoColletivo} className='logoColletivo'/>
+                <img src={OnibusColletivo} className='onibusColletivo'/>
+                <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
+                <span className='btn-colletivo'>
+                    <p>Ir para o site</p>  
+                </span>
+
+                
             <footer className="rodape container">
             </footer>
         </div>
