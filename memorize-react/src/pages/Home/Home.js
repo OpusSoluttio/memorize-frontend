f56e0bcbca0e8ba515448bc56c987784a6f83825
nadfelipe <48352348+nadfelipe@users.noreply.github.com>
2020-06-15 22:51:55 -0300

    up

--- memorize-react/src/pages/Home/Home.js
+++ memorize-react/src/pages/Home/Home.js
@@ -1,74 +1,88 @@
-import React from "react";
+import React from 'react';
 import Cerebro from '../../assets/img/cerebro.png'
 import LogoMemorize from '../../assets/img/logo-memorize.png'
 import LogoColletivo from '../../assets/img/logo-colletivo.png'
 import OnibusColletivo from '../../assets/img/onibus-colletivo.png'
+import LogoFooter from '../../assets/img/logofooter.png';
+import MemorizeJogo from '../../assets/img/memorize-jogo.png';
 import BtnIniciar from '../../assets/img/btn-iniciar.png'
-import {Link} from "react-router-dom";
-import "./Home.css";
+import {Link} from 'react-router-dom';
+import './Home.css';
+import PageProgress from 'react-page-progress';
 
 export default function Home(){
     return(
-        <div className="Home">
+        <div className='Home'>
             <section className='header'>
-                <header className="main-header-container">
-                    <nav className="main-nav">
-                        <a href="#">Como jogar</a>
-                        <a href="#">Sobre</a>
-                        <a href="#">Colletivo Design</a>
+                <header className='main-header container'>
+                    <div className='blur-nav'></div>
+                    <nav className='main-nav'>
+                        <a href='#comojogar'>Como Jogar</a>
+                        <a href='#sobre'>Sobre</a>
+                        <a href='#colletivo'>Colletivo Design</a>
                     </nav>
-
-                    <div className='home-principal'>
-                        <span className='secreto'></span>
-                        <div className='home-logo'>
+                    <div className='page-progress'>
+                        <PageProgress color={'#FF7D76'} height={'5px'}/>
+                    </div>
+                    <div className='home-principal content'>
+                        <div className='home-section'>
                             <img src={LogoMemorize}/>
-                            <p className='sub-logo'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra rutrum tempor!</p> 
+                            <p className='texto-home-section'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra rutrum tempor!</p> 
                             <br/>
-                            <span className="botao-jogar">
+                            <span className='botao-jogar-home'>
                                 <img src={BtnIniciar}/>
                                 <p>INICIAR</p>
-                            </span>               
-                        </div>
+                            </span>   
+                        </div>       
                     </div>
-                    
-                    
                 </header>
             </section>
-            <section className="como-jogar-container">
-                <h2>Como Jogar</h2>
-                <div>
-                    <ul>
-                        <li>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci</li>
-                        <li>2.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
-                        <li>3.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
-                    </ul>
-                    {/* adicionar imagem de como jogar */}
-                    {/* <img /> */}
+            <section className='como-jogar container' id='comojogar'>
+                <div className='como-jogar content' >
+                    <div className='title-comojogar'>
+                        <h2>Como Jogar</h2>
+                        <hr/>
+                    </div>
+                    <div className='comojogar-section'>
+                        <ul>
+                            <li>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci</li>
+                            <li>2.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
+                            <li>3.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
+                        </ul>
+                        <img src={MemorizeJogo}/>
+                    </div>
+                    <span className='botao-jogar-comojogar'>
+                        <img src={BtnIniciar}/>
+                        <p>JOGAR AGORA</p>
+                    </span>    
                 </div>
-                <span className="botao-jogar">
-                    {/* <img /> */}
-                    <p>Iniciar</p>
-                </span>    
             </section>
-            <section className="sobre-container">
-                <h1>Sobre</h1>
-                <h1>O que é?</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
-                <h1>Quem somos?</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+            <section className='sobre container' id='sobre'>
+                <div className='sobre content'>
+                    <h1>Sobre</h1>
+                    <h1>O que é?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                    <h1>Quem somos?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                </div>
             </section>
-            <section className="colletivo-container">
+            <section className='colletivo container' id='colletivo'>
+                <div className='colletivo content'>
+                    <p>Este projeto é oferecido por</p>
+                    <img src={LogoColletivo} className='logoColletivo'/>
+                    <img src={OnibusColletivo} className='onibusColletivo'/>
+                    <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
+                    <span className='btn-colletivo'>
+                        <p>Ir para o site</p>  
+                    </span>
+                </div>
             </section>
-                <p>Este projeto é oferecido por</p>
-                <img src={LogoColletivo} className='logoColletivo'/>
-                <img src={OnibusColletivo} className='onibusColletivo'/>
-                <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
-                <span className='btn-colletivo'>
-                    <p>Ir para o site</p>  
-                </span>
-
-                
-            <footer className="rodape container">
+            <footer className='rodape'>
+                {/* TODO: ADICIONAR LOGO BRANCO */}
+                <div>
+                    {/* TODO: FAZER LINHA */}
+                </div>
+                <p>©2020 Opus Soluttio. Todos os direitos reservados</p>
             </footer>
         </div>
     )
