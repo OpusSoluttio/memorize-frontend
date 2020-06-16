39d9930be188242e6bfdfc2c4cb0480b66fc6af1
DiegoColmenero <dicolmenero@terra.com.br>
2020-06-16 11:43:42 -0300

    home estilizada

--- memorize-react/src/pages/Home/Home.js
+++ memorize-react/src/pages/Home/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Cerebro from '../../assets/img/cerebro.png'
 import LogoMemorize from '../../assets/img/logo-memorize.png'
 import LogoColletivo from '../../assets/img/logo-colletivo.png'
-import OnibusColletivo from '../../assets/img/onibus-colletivo.png'
+import OnibusColletivo from '../../assets/img/onibus-colletivo.gif'
 // import LogoFooter from '../../assets/img/logofooter.png';
 import MemorizeJogo from '../../assets/img/memorize-jogo.png';
+import SetaColletivo from '../../assets/icons/arrow-icon.png'
 import BtnIniciar from '../../assets/img/btn-iniciar.png'
 import {Link} from 'react-router-dom';
 import './Home.css';
@@ -42,7 +43,6 @@ export default function Home(){
                 <div className='como-jogar content' >
                     <div className='title-comojogar'>
                         <h2>Como Jogar</h2>
-                        <hr/>
                     </div>
                     <div className='comojogar-section'>
                         <ul>
@@ -52,7 +52,7 @@ export default function Home(){
                         </ul>
                         <img src={MemorizeJogo}/>
                     </div>
-                    <span className='botao-jogar-comojogar'>
+                    <span href='#' className='botao-jogar-comojogar'>
                         <img src={BtnIniciar}/>
                         <p>JOGAR AGORA</p>
                     </span>    
@@ -60,22 +60,35 @@ export default function Home(){
             </section>
             <section className='sobre container' id='sobre'>
                 <div className='sobre content'>
-                    <h1>Sobre</h1>
-                    <h1>O que é?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
-                    <h1>Quem somos?</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                    <h2 id="sobre-title">Sobre</h2>
+                    <div class="center-line"></div>
+
+                    <div className="titulo-e-texto">
+                        <h2>O que é?</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                    </div>
+                    <div className="titulo-e-texto inverte">
+                        <h2>Quem somos?</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                    </div>
                 </div>
             </section>
             <section className='colletivo container' id='colletivo'>
-                <div className='colletivo content'>
-                    <p>Este projeto é oferecido por</p>
-                    <img src={LogoColletivo} className='logoColletivo'/>
-                    <img src={OnibusColletivo} className='onibusColletivo'/>
-                    <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
-                    <span className='btn-colletivo'>
-                        <p>Ir para o site</p>  
-                    </span>
+                <div className='colletivo-content content'>
+                    <div className='colletivo-titulo'>
+                        <p>Este projeto é oferecido por</p>
+                    </div>
+                    <div className='colletivo-banner'>
+                        <img src={LogoColletivo} className='logoColletivo'/>
+                    </div>
+                    <div className='colletivo-informacao'>
+                        <img src={OnibusColletivo} className='onibusColletivo'/>
+                        <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
+                    </div>
+                    <a href="https://colletivo.com.br/" target="_blank" className='btn-colletivo'>
+                        <p>Ir para o site</p>
+                        <img src={SetaColletivo} alt="setaGiratoria"/>
+                    </a>
                 </div>
             </section>
             <Footer />
