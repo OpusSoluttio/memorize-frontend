185384954608e6ff379e9f2fe907d8f20b65550e
lukkanog <lukanog@hotmail.com>
2020-06-23 08:22:14 -0300

    animacoes no header do main

--- memorize-react/src/pages/Home/Home.js
+++ memorize-react/src/pages/Home/Home.js
@@ -46,95 +46,85 @@ export default function Home() {
                     <div className='page-progress'>
                         <PageProgress color={'#FF7D76'} height={'5px'} />
                     </div>
-                    <div className='home-principal content'>
-                        <div className='home-section'>
+                    <Anime
+                        in
+                        appear
+                        duration={0}
+                        delay={0}
+                        onEntered={{ translateY: [-100, 0], opacity: [0, 1], duration:2500 }}
+                        onExited={{ translateY: -30, opacity: 0 }}
+                    >
 
-                            <img className='logo' src={LogoMemorize} />
-                            <p className='texto-home-section'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra rutrum tempor!</p>
-                            <br />
-                            <span className='botao-jogar-home'>
-                                <img src={BtnIniciar} />
-                                <p>INICIAR</p>
-                            </span>
+                        <div className='home-principal content'>
+                            <div className='home-section'>
+
+                                <img className='logo' src={LogoMemorize} />
+                                <p className='texto-home-section'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra rutrum tempor!</p>
+                                <br />
+                                <span className='botao-jogar-home'>
+                                    <img src={BtnIniciar} />
+                                    <p>INICIAR</p>
+                                </span>
+                            </div>
                         </div>
-                    </div>
+                    </Anime>
                 </header>
             </section>
 
             <section className='como-jogar container' id='comojogar'>
-                <Anime
-                    in
-                    appear
-                    mountOnEnter={true}
-                    duration={3000}
-                    onExited={{ translateY: -20, opacity: 0 }}
-                    onEntered={{ translateY: [-20, 0], opacity: [0, 1] }}
 
-                >
-                    <div className='como-jogar content' >
-                        <div className='title-comojogar'>
-                            <h2>Como Jogar</h2>
-                        </div>
-                        <div className='comojogar-section'>
-                            <ul>
-                                <li>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci</li>
-                                <li>2.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
-                                <li>3.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
-                            </ul>
-                            <img className='memorize-jogo' src={MemorizeJogo} />
-                        </div>
-                        <span href='#' className='botao-jogar-comojogar'>
-                            <img src={BtnIniciar} />
-                            <p>JOGAR AGORA</p>
-                        </span>
+                <div className='como-jogar content' >
+                    <div className='title-comojogar'>
+                        <h2>Como Jogar</h2>
                     </div>
-                </Anime>
-            </section>
-            <section className='sobre container' id='sobre'>
-                <div className='sobre content'>
-                    <h2 id="sobre-title">Sobre</h2>
-                    <div class="center-line"></div>
-
-                    <div className="titulo-e-texto">
-                        <h2>O que é?</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                    <div className='comojogar-section'>
+                        <ul>
+                            <li>1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci</li>
+                            <li>2.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
+                            <li>3.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum urna orci, aliquam vel sagittis a, molestie lobortis nibh.</li>
+                        </ul>
+                        <img className='memorize-jogo' src={MemorizeJogo} />
                     </div>
-                    <div className="titulo-e-texto inverte">
-                        <h2>Quem somos?</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
-                    </div>
+                    <span href='#' className='botao-jogar-comojogar'>
+                        <img src={BtnIniciar} />
+                        <p>JOGAR AGORA</p>
+                    </span>
                 </div>
             </section>
-            <section className='colletivo container' id='colletivo'>
-                <Anime
-                    in
-                    appear
-                    mountOnEnter={true}
-                    duration={3000}
-                    onEntered={{ translateY: [-20, 0], opacity: [0, 1] }}
-                    onExited={{ translateY: -20, opacity: 0 }}
-                    easing="easeOutCubic"
+        <section className='sobre container' id='sobre'>
+            <div className='sobre content'>
+                <h2 id="sobre-title">Sobre</h2>
+                <div class="center-line"></div>
 
-                >
-                    <div className='colletivo-content content'>
-                        <div className='colletivo-titulo'>
-                            <p>Este projeto é oferecido por</p>
-                        </div>
-                        <div className='colletivo-banner'>
-                            <img src={LogoColletivo} className='logoColletivo' />
-                        </div>
-                        <div className='colletivo-informacao'>
-                            <img src={OnibusColletivo} className='onibusColletivo' />
-                            <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
-                        </div>
-                        <a href="https://colletivo.com.br/" target="_blank" className='btn-colletivo'>
-                            <p>Ir para o site</p>
-                            <img src={SetaColletivo} alt="setaGiratoria" />
-                        </a>
+                <div className="titulo-e-texto">
+                    <h2>O que é?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                </div>
+                <div className="titulo-e-texto inverte">
+                    <h2>Quem somos?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam et risus mollis, accumsan purus sit amet, pretium enim. Aliquam erat volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent vel nisl ut odio efficitur ullamcorper ut at mauris</p>
+                </div>
+            </div>
+        </section>
+        <section className='colletivo container' id='colletivo'>
+                <div className='colletivo-content content'>
+                    <div className='colletivo-titulo'>
+                        <p>Este projeto é oferecido por</p>
                     </div>
-                </Anime>
-            </section>
-            <Footer />
-        </div>
+                    <div className='colletivo-banner'>
+                        <img src={LogoColletivo} className='logoColletivo' />
+                    </div>
+                    <div className='colletivo-informacao'>
+                        <img src={OnibusColletivo} className='onibusColletivo' />
+                        <p>Conhecido por desenvolver e transformar design de forma estratégica para grandes empresas à startups, o Colletivo Design trabalha com branding, design gráfico e digital, ilustração, embalagem, sinalização, instalações digitais para publicidade, filmes, música e entretenimento em quase 15 anos de história.</p>
+                    </div>
+                    <a href="https://colletivo.com.br/" target="_blank" className='btn-colletivo'>
+                        <p>Ir para o site</p>
+                        <img src={SetaColletivo} alt="setaGiratoria" />
+                    </a>
+                </div>
+        </section>
+        <Footer />
+        </div >
     )
 }
